edf1879413fa4ce2fcd6de96a52a9c6752ba634d
lmy6268 <70847610+lmy6268@users.noreply.github.com>
2021-06-14 05:15:09 +0900

    다이어로그 탈출시 저장할 것 인지 확인함.

--- app/src/main/java/com/example/daylight/customDialog.java
+++ app/src/main/java/com/example/daylight/customDialog.java
@@ -1,8 +1,10 @@
 package com.example.daylight;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,6 +13,8 @@ import android.graphics.drawable.DrawableContainer;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.StateListDrawable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
@@ -45,6 +49,7 @@ public class customDialog extends Dialog {
     DisplayMetrics disp = getContext().getResources().getDisplayMetrics();
     int deviceWidth = disp.widthPixels;
     int deviceHeight = disp.heightPixels;
+    Boolean isSaved = true;
 
     public customDialog(@NonNull Context context) {
         super(context);
@@ -57,36 +62,79 @@ public class customDialog extends Dialog {
 
         Button btnSave, btnImage, btnBgm, btnEmo;
         TextView tvDate;
-        EditText editTitle,editDiary;
+        EditText editTitle, editDiary;
 
         String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
 
         final View innerView = getLayoutInflater().inflate(R.layout.dialog_diary, null);
         customDialog octDialog = new customDialog(getContext());
-        tvDate=innerView.findViewById(R.id.tvDate);
-        tvDate.append(" "+date);
+        tvDate = innerView.findViewById(R.id.tvDate);
+        tvDate.append(" " + date);
         btnSave = innerView.findViewById(R.id.btnSave);
         btnImage = innerView.findViewById(R.id.btnImage);
         btnBgm = innerView.findViewById(R.id.btnBgm);
         btnEmo = innerView.findViewById(R.id.btnEmo);
         editDiary = innerView.findViewById(R.id.editDiary);
         octDialog.setContentView(innerView);
-        octDialog.setCanceledOnTouchOutside(false);// 다이알로그 바깥영역 터치시, 다이알로그 닫힘
+        octDialog.setCanceledOnTouchOutside(true);// 다이알로그 바깥영역 터치시, 다이알로그 닫
         octDialog.setCancelable(true);//Back키로 다이어로그 닫기
+        octDialog.setOnCancelListener(new OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                if (isSaved == false) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                            .setTitle("저장")
+                            .setMessage("저장 하시겠습니까?")
+                            .setPositiveButton("저장", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    try {
+                                        saveDiary(editDiary.getText().toString(), date);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            })
+                            .setNegativeButton("저장안함", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                    dialog.dismiss();
+                                }
+
+                            });
+                    AlertDialog alertDialog = builder.create();
+
+                    alertDialog.show();
+                }
+            }
+        });
         WindowManager.LayoutParams params = octDialog.getWindow().getAttributes();
         params.width = (int) (deviceWidth * 0.9);
         params.height = (int) (deviceHeight * 0.7);
         octDialog.getWindow().setAttributes((WindowManager.LayoutParams) params);
         try {
             editDiary.setText(loadDiary(date));
-            loadMood(date,btnEmo);
+            loadMood(date, btnEmo);
         } catch (FileNotFoundException e) {
         } catch (IOException e) {
         } finally {
             octDialog.show();
         }
-        btnImage.setOnClickListener(v->{
+        editDiary.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                isSaved=false;
+            }
+        });
+        btnImage.setOnClickListener(v -> {
 //            LayerDrawable layerDrawable = (LayerDrawable) new LayerDrawable();
 //            GradientDrawable gradientDrawable = (GradientDrawable) layerDrawable
 //                    .findDrawableByLayerId(R.id.gradientDrawble);
@@ -100,7 +148,7 @@ public class customDialog extends Dialog {
             }
         });
         btnEmo.setOnClickListener(v -> {
-            showEmolog(date,btnEmo);
+            showEmolog(date, btnEmo);
         });
 
     }
@@ -108,10 +156,10 @@ public class customDialog extends Dialog {
     public void saveDiary(String text, String Date) throws IOException {
         FileOutputStream fos = null;
 
-        String path=findPath(Date);
+        String path = findPath(Date);
         try {
 
-            fos = new FileOutputStream(path+"/"+Date+".txt");
+            fos = new FileOutputStream(path + "/" + Date + ".txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -120,24 +168,25 @@ public class customDialog extends Dialog {
         dos.flush();
         dos.close();
         Toast.makeText(getContext(), "저장되었습니다", Toast.LENGTH_SHORT).show();
+        isSaved=true;
     }
 
     public String loadDiary(String Date) throws IOException {
         String result;
         FileInputStream fis = null;
-        String path=findPath(Date);
-        fis = new FileInputStream(path+"/"+Date+".txt");
+        String path = findPath(Date);
+        fis = new FileInputStream(path + "/" + Date + ".txt");
         DataInputStream dis = new DataInputStream(fis);
         result = dis.readUTF();
         return result;
     }
 
-    public void  showEmolog(String Date,Button parentEmo)//감정 선택 다이어로그 창 띄움
+    public void showEmolog(String Date, Button parentEmo)//감정 선택 다이어로그 창 띄움
     {
         final View innerView = getLayoutInflater().inflate(R.layout.emolog, null);
         Button[] btnEmo = {innerView.findViewById(R.id.btnEmo1), innerView.findViewById(R.id.btnEmo2), innerView.findViewById(R.id.btnEmo3), innerView.findViewById(R.id.btnEmo4),
                 innerView.findViewById(R.id.btnEmo5), innerView.findViewById(R.id.btnEmo6)};
-        String[] color={"#00FF00","#FF0000","#0000FF","#FFA500","#FFC0CB","#FFFF00"};//버튼의 색 목록
+        String[] color = {"#00FF00", "#FF0000", "#0000FF", "#FFA500", "#FFC0CB", "#FFFF00"};//버튼의 색 목록
 
         customDialog octDialog = new customDialog(getContext());
         octDialog.setContentView(innerView);
@@ -153,8 +202,8 @@ public class customDialog extends Dialog {
                 @Override
                 public void onClick(View v) {
                     try {
-                        saveMood(index,btnEmo[index].getTag().toString(),color[index],Date);
-                        Log.d("color",color[index]);
+                        saveMood(index, btnEmo[index].getTag().toString(), color[index], Date);
+                        Log.d("color", color[index]);
                         parentEmo.setBackgroundColor(Color.parseColor(color[index]));
                         octDialog.dismiss();
                     } catch (IOException e) {
@@ -167,17 +216,17 @@ public class customDialog extends Dialog {
         octDialog.show();
     }
 
-    public void saveMood(int index,String mood,String color,String Date) throws IOException {
+    public void saveMood(int index, String mood, String color, String Date) throws IOException {
         FileOutputStream fos = null;
 
-        String path=findPath(Date);
+        String path = findPath(Date);
         try {
-            fos = new FileOutputStream(path+"/"+Date+"Mood.txt");
+            fos = new FileOutputStream(path + "/" + Date + "Mood.txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         DataOutputStream dos = new DataOutputStream(fos);
-        String text= index +":"+mood +":" +color ;
+        String text = index + ":" + mood + ":" + color;
         try {
             dos.writeUTF(text);
         } catch (IOException e) {
@@ -187,38 +236,39 @@ public class customDialog extends Dialog {
         dos.close();
         Toast.makeText(getContext(), "저장되었습니다", Toast.LENGTH_SHORT).show();
     }
-    public void loadMood(String Date,Button btnEmo) throws FileNotFoundException{
+
+    public void loadMood(String Date, Button btnEmo) throws FileNotFoundException {
         String result;
         FileInputStream fis = null;
-        String path=findPath(Date);
-        fis= new FileInputStream(path+"/"+Date+"Mood.txt");
+        String path = findPath(Date);
+        fis = new FileInputStream(path + "/" + Date + "Mood.txt");
         DataInputStream dis = new DataInputStream(fis);
         try {
             result = dis.readUTF();
             String i = result.split(":")[2].trim();
             btnEmo.setBackgroundColor(Color.parseColor(i));
-            Log.d("dd",i);
-        }catch (IOException e ){
+            Log.d("dd", i);
+        } catch (IOException e) {
             btnEmo.setBackgroundColor(Color.WHITE);
         }
 
     }
-    public String findPath(String Date){
-        String path = context.getFilesDir().getAbsolutePath() +"/"+Date;//폴더 경로
+
+    public String findPath(String Date) {
+        String path = context.getFilesDir().getAbsolutePath() + "/" + Date;//폴더 경로
         File Folder = new File(path);
 
         // 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
         if (!Folder.exists()) {
-            try{
+            try {
                 Folder.mkdir(); //폴더 생성합니다.
                 System.out.println("폴더가 생성되었습니다.");
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 e.getStackTrace();
             }
-        }else {
+        } else {
             System.out.println("이미 폴더가 생성되어 있습니다.");
         }
-        return  path;
+        return path;
     }
 }
