074b9c9d7270b32dbc1ede1f0b1d44de66bf4e25
yea-rim <sis04026@naver.com>
2021-06-13 23:03:31 +0900

    BGM,, 랜덤은 안 돼요 ㅠ

--- app/src/main/java/com/example/daylight/customDialog.java
+++ app/src/main/java/com/example/daylight/customDialog.java
@@ -1,7 +1,10 @@
 package com.example.daylight;
 import android.app.Dialog;
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 
@@ -18,5 +21,30 @@ public class customDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layoutId);
+
+        Button b = findViewById(R.id.randomBgm);
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Context c = v.getContext();
+
+                MediaPlayer m = MediaPlayer.create(c , R.raw.bg1 );
+                m.start();
+
+                m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp)
+                    {
+                        mp.stop();
+                        mp.release();
+                    }
+                });
+
+
+            }
+        });
     }
 }
+
